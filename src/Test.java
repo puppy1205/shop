c11c88d0d97eb9f081d636d591e3d6647e2229b7
lenovo <11>
2020-12-26 15:35:38 +0800

    创建Test

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("请输入用户名");
+        Scanner sc=new Scanner(System.in);
+        String
+    }
+}
